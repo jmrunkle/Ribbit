bdd6cfe18e7d0c71733430e0af87707d79b75ec2
runkle89 <jmrunkle@gmail.com>
2015-09-10 22:38:01 -0700

    Add the file contents to the message using helper methods and classes

--- app/src/main/java/com/teamtreehouse/ribbit/RecipientsActivity.java
+++ app/src/main/java/com/teamtreehouse/ribbit/RecipientsActivity.java
@@ -17,6 +17,7 @@ import android.widget.TextView;
 
 import com.parse.FindCallback;
 import com.parse.ParseException;
+import com.parse.ParseFile;
 import com.parse.ParseObject;
 import com.parse.ParseQuery;
 import com.parse.ParseRelation;
@@ -148,7 +149,20 @@ public class RecipientsActivity extends AppCompatActivity {
         message.put(ParseConstants.KEY_RECIPIENT_IDS, getRecipientIds());
         message.put(ParseConstants.KEY_FILE_TYPE, mFileType);
 
-        return message;
+        byte[] fileBytes = FileHelper.getByteArrayFromFile(this, mMediaUri);
+
+        if (fileBytes == null) {
+            return null;
+        } else {
+            if (mFileType.equals(ParseConstants.TYPE_IMAGE)) {
+                fileBytes = FileHelper.reduceImageForUpload(fileBytes);
+            }
+
+            String fileName = FileHelper.getFileName(this, mMediaUri, mFileType);
+            ParseFile file = new ParseFile(fileName, fileBytes);
+            message.put(ParseConstants.KEY_FILE, file);
+            return message;
+        }
     }
 
     private ArrayList<String> getRecipientIds() {
